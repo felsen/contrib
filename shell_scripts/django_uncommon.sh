a7cf3c040b258dc34521e0ec05fc33bb4825be7a
felsen <felsen88@gmail.com>
2015-10-09 06:56:19 +0530

    Modified with counter

--- shell_scripts/django_uncommon.sh
+++ shell_scripts/django_uncommon.sh
@@ -1,14 +1,10 @@
 #!bin/bash
 
-
 if [ -e manage.py ]; then
-    i=0;
     for file in behaviours.py constants.py decorators.py fields.py factories.py helpers.py managers.py signals.py utils.py viewmixins.py
     do
         if [ ! -f $file ]; then
-            #echo $i
-            #echo $content[$i] >> $file
-            #touch $file
+            touch $file
         fi
     done    
     if [ ! -d db ]; then
